c9fefb00fd5f0056d1fc5c944018f29d05c93e1a
Vinay Sagar Sharma <vinaysagar971995@gmail.com>
2020-09-04 23:35:14 +0530

    created a static list and a static card and used a mix between material UI react and styled components

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import TrelloList from "./TrelloList";
 
 function App() {
   return (
     <div className="App">
       <h2>Hello VINAY!</h2>
+      <TrelloList title="Todo" />
     </div>
   );
 }
---
+++ src/components/TrelloList.js
@@ -0,0 +1,20 @@
+import React from "react";
+import TrelloCard from "./TrelloCard";
+
+const TrelloList = ({ title }) => {
+  return (
+    <div style={styles.container}>
+      <h3>{title}</h3>
+      <TrelloCard />
+    </div>
+  );
+};
+const styles = {
+  container: {
+    backgroundColor: "#ccc",
+    borderRadius: 3,
+    width: 300,
+    padding: 8,
+  },
+};
+export default TrelloList;
